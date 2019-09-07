10ea503c8af97e03efd81300e7bffa6aba687e53
Alex Garland <alex.d.garland@gmail.com>
2019-09-08 00:58:57 +0100

    Refactor code
    Focussed on separating as much logic as possible out from the direct HTTP interface

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -1,70 +1,29 @@
 #! /usr/bin/env python3.6
 
-# TODO - this needs unit tests and proper logging
+# TODO - this needs proper logging
 
-from getpass import getuser
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from os.path import split, dirname, join
-
-from jinja2 import Template
+from os.path import join
 
 from .config import ServerConfig
-
-RESOURCE_ROOT = dirname(__file__)
-INDEX_TEMPLATE_FILE = f"{RESOURCE_ROOT}/pages/index.j2"
+from .request_path import ParsedRequestPath
+from .resource_fetching import get_resource_fetcher, RESOURCE_ROOT
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    @property
-    def template_params(self):
-        return {"username": getuser()}
-
-    @property
-    def index_page(self):
-        with open(INDEX_TEMPLATE_FILE, "r") as f:
-            template = Template(f.read())
-        return template.render(self.template_params)
-
-    def _content_get(self, content_type, body):
-        self.send_response(200)
-        self.send_header("Content-type", content_type)
-        self.end_headers()
-        self.wfile.write(body)
-
     def do_GET(self):
-        print(f"Handling path {self.path}")
-
-        # if self.path == "/":
-        #     self._content_get("text/html", self.index_page.encode())
-
-        directory = split(self.path)[0]
-        # TODO full split here to dir, filename, filetype (MIME) (as a data object)
-
-        # TODO - ultimately we are looking at behaviour like:
-        #   If "/" serve index page (using text/html class with fixed param)
-        #   Otherwise:
-        #       Check if path exists - if not, 404
-        #       Otherwise load from file and then:
-        #       If exists and is pages/*.html, render, encode and serve as text/html
-        #           (for the moment assume will always be a .j2 file with same dictionary)
-        #       If exists and is images/*.filetype, serve as image/filetype with no further rendering or encoding
-
-        resource_path = RESOURCE_ROOT + self.path
-
-        if directory == '/images':
-            with open(resource_path, "rb") as f:
-                body_image = f.read()
-            self._content_get("image/gif", body_image)
-        else:
-            self._content_get("text/html", self.index_page.encode())
+        parsed_path = ParsedRequestPath(self.path)
+        response_details = get_resource_fetcher(parsed_path).get_response()
+        self.send_response(response_details.response_code)
+        self.send_header("Content-type", response_details.content_type)
+        self.end_headers()
+        self.wfile.write(response_details.body)
 
 
 def main():
-
     server_config = ServerConfig.create(join(RESOURCE_ROOT, "server_defaults.yaml"))
     print(server_config)
-
     httpd = HTTPServer(server_config.address, SimpleHTTPRequestHandler)
     httpd.serve_forever()
 
---
+++ webserver/request_path.py
@@ -0,0 +1,19 @@
+from posixpath import basename, dirname
+from os.path import splitext
+
+
+INDEX_PAGE = "index.html"
+
+
+class ParsedRequestPath(object):
+
+    def __init__(self, request_path):
+        self.request_path = request_path
+
+        # TODO Stripping off the leading "/" here is really important when we later try and join paths -
+        # TODO  need to ensure that gets captured in tests as and when we write some!
+        self.request_dir = dirname(self.request_path)[1:]
+
+        name = basename(self.request_path)
+        self.request_filename = INDEX_PAGE if name == "" else name  # Poor man's redirect ;-)
+        self.filename_root, self.file_ext = splitext(self.request_filename)
